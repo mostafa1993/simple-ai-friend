cbcf65d6e843ce5858a9992ef1e91e60116518e5
mostafa1993 <mahdavi.mst@gmail.com>
2025-05-09 17:24:55 +0200

    Initialize chatbot project with core files: added .gitignore, chatbot logic, color utility, LLM handler, spinner for loading animation, and configuration for dependencies and formatting.

---
+++ chatbot.py
@@ -0,0 +1,33 @@
+from llm_handler import SimpleLLMHandler
+from colors import Color
+from spinner import Spinner
+
+
+color = Color()
+
+print(color.cyan("🤖 Welcome to Your Simple AI Assistant!", bold=True))
+print(color.yellow("Type 'exit' to end the chat.\n"))
+
+llm = SimpleLLMHandler(
+    # provider="openai",
+    # model="gpt-4o-mini",
+    provider="ollama",
+    model="gemma2:2b",
+    name="Kishmish",
+    role="Maltese dog",
+)
+
+while True:
+    user_input = input(color.blue("You: ", bold=True))
+    if user_input.lower() == "exit":
+        print(color.cyan("👋 Goodbye!"))
+        break
+
+    try:
+        with Spinner(color=color):
+            reply = llm.generate(user_input)
+
+        print(color.magenta("Assistant: ", bold=True) + reply)
+    except Exception as e:
+        print(color.red(f"Error: {e}"))
+        break
